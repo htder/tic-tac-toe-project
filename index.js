c156e3994456865da58bd5c52b05bee287d3d306
Henry Derbyshire <henry@htderbyshire.com>
2022-01-14 14:37:44 +0000

    added more html related to players and winning

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 "use strict";
+const Player = (name, type) => {
+  const gamePiece = type;
+  const getName = () => name;
+  const getType = () => type;
 
-const gameboard = (() => {
+  return { getName, getType };
+};
+
+const player1 = Player("player1", "0");
+const player2 = Player("player2", "X");
+
+const gameboard = ((player1, player2) => {
+  let playerTurn = true;
+  const players = [player1, player2];
   const board = new Array(9).fill("", 0, 9);
+  const gameboardCells = Array.from(document.querySelectorAll(".content"));
+
+  const getCells = () => {
+    return gameboardCells;
+  };
   const getBoard = () => {
     return board;
   };
@@ -9,42 +26,68 @@ const gameboard = (() => {
     board[index] = type;
   };
 
-  return { getBoard, setCell };
-})();
+  const takeTurn = () => {
+    gameboardCells.forEach((cell) =>
+      cell.parentNode.addEventListener("click", () => {
+        console.log(cell);
+        const [index] = /\d+/.exec(cell.id);
+        if (board[index] === "") {
+          const type = players[playerTurn ? 0 : 1].getType();
+          cell.textContent = type;
+          playerTurn = !playerTurn;
+          board[index] = type;
+          const winner = checkWinner();
+          console.log(`${winner} has won`);
+        }
+      })
+    );
+  };
+
+  const checkWinner = () => {
+    for (let i = 0; i < board.length; i += 3) {
+      if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        return board[i];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+        return board[i];
+      }
+    }
+    if (board[0] === board[4] && board[0] === board[8]) {
+      return board[2];
+    }
+    if (board[2] === board[4] && board[2] === board[6]) {
+      return board[2];
+    }
+    const isArrayFull = board.filter((x) => x !== "");
+    if (isArrayFull.length !== 9) {
+      return "";
+    } else {
+      return "draw";
+    }
+  };
+
+  return { getBoard, setCell, getCells, takeTurn };
+})(player1, player2);
 
 const game = (() => {})();
 
 const displayController = (() => {
-  const gameboardCells = Array.from(document.querySelectorAll(".content"));
-  const fillCells = (board) => {
-    gameboardCells.forEach((cell) => {
+  const fillCells = (gameboard) => {
+    gameboard.getCells().forEach((cell) => {
       const [index] = /\d+/.exec(cell.id);
-      cell.textContent = board[index];
+      cell.textContent = gameboard.getBoard()[index];
     });
   };
   return { fillCells };
 })();
 
-const Player = (name, type) => {
-  const gamePiece = type;
-  const getName = () => name;
-  const getType = () => type;
-
-  const takeTurn = () => {
-    console.log(`${name} it is your turn!`);
-  };
-
-  return { getName, getType, takeTurn };
-};
-
-// const player1 = Player("henry", "x");
-// player1.takeTurn();
-
 gameboard.setCell(8, "X");
 gameboard.setCell(1, "0");
 gameboard.setCell(0, "0");
-gameboard.setCell(5, "X");
 gameboard.setCell(2, "X");
 gameboard.setCell(6, "0");
 console.log(gameboard.getBoard());
-displayController.fillCells(gameboard.getBoard());
+displayController.fillCells(gameboard);
+gameboard.takeTurn();
