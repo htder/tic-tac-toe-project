0672ee33b1305767d31f1b73ffad6e347f75c38b
Henry Derbyshire <henry@htderbyshire.com>
2022-01-14 17:32:22 +0000

    finished modal to take players names

--- index.js
+++ index.js
@@ -15,20 +15,115 @@ const Player = (name, type, position) => {
   return { getName, getType, setDomText };
 };
 
-const player1 = Player("Player 1", "X", 1);
-player1.setDomText();
-const player2 = Player("Player 2", "0", 2);
-player2.setDomText();
+let player1;
+let player2;
 
-const gameboard = ((player1, player2) => {
+const game = ((player1, player2) => {})(player1, player2);
+
+const displayController = (() => {
+  const modal = document.querySelector(".modal");
+  const play = document.querySelector(".submit");
+  const form = document.getElementById("form");
+  const player1NameEl = document.querySelector("#player1");
+  const player2NameEl = document.querySelector("#player2");
+
+  const fillCells = () => {
+    gameboard.getCells().forEach((cell) => {
+      const [index] = /\d+/.exec(cell.id);
+      cell.textContent = gameboard.getBoard()[index];
+    });
+  };
+
+  const validateForm = () => {
+    let player1Valid = modalHelper.checkName(player1NameEl),
+      player2Valid = modalHelper.checkName(player2NameEl);
+
+    let isFormValid = player1Valid && player2Valid;
+    if (isFormValid) {
+      player1 = Player(player1NameEl.value.trim(), "X", 1);
+      player2 = Player(player2NameEl.value.trim(), "O", 2);
+      player1.setDomText();
+      player2.setDomText();
+      gameboard.setPlayers(player1, player2);
+      modalHelper.clearForm(form);
+      modalHelper.hideModal(modal);
+    }
+  };
+
+  const playListener = () => {
+    play.addEventListener("click", (event) => {
+      event.preventDefault();
+      validateForm();
+    });
+  };
+
+  return { fillCells, playListener };
+})();
+
+const modalHelper = (() => {
+  const isRequired = (value) => {
+    return value === "";
+  };
+
+  const showError = (input, message) => {
+    input.classList.remove("success");
+    input.classList.add("error");
+
+    const formField = input.parentElement;
+    const error = formField.querySelector("small");
+    error.textContent = message;
+  };
+
+  const showSuccess = (input) => {
+    const formField = input.parentElement;
+    input.classList.remove("error");
+    input.classList.add("success");
+
+    const error = formField.querySelector("small");
+    error.textContent = "";
+  };
+
+  const checkName = (element) => {
+    let valid = false;
+    const title = element.value.trim();
+    if (isRequired(title)) {
+      showError(element, "Name cannot be empty.");
+    } else {
+      showSuccess(element);
+      valid = true;
+    }
+    return valid;
+  };
+
+  const clearForm = (form) => {
+    form["player1"].value = "";
+    form["player2"].value = "";
+  };
+
+  const hideModal = (modal) => {
+    modal.style.display = "none";
+  };
+
+  const showModal = (modal) => {
+    modal.style.display = "block";
+    document.querySelector(".focus").focus();
+  };
+
+  return { checkName, clearForm, hideModal, showModal };
+})();
+
+displayController.playListener();
+
+const gameboard = (() => {
   let playerTurn = true;
-  const players = [player1, player2];
-  const board = new Array(9).fill("", 0, 9);
+  let players;
+  let board = new Array(9).fill("", 0, 9);
   const gameboardCells = Array.from(document.querySelectorAll(".content"));
   const winnerContainer = document.querySelector(".winner");
 
   const clearBoard = () => {
     board = new Array(9).fill("", 0, 9);
+    displayController.fillCells();
   };
 
   const getCells = () => {
@@ -45,6 +140,8 @@ const gameboard = ((player1, player2) => {
     const cell = Array.from(event.target.childNodes)[1];
     const [index] = /\d+/.exec(cell.id);
     if (board[index] === "") {
+      console.log(players);
+      console.log(playerTurn);
       const type = players[playerTurn ? 0 : 1].getType();
       cell.textContent = type;
       playerTurn = !playerTurn;
@@ -54,7 +151,18 @@ const gameboard = ((player1, player2) => {
         return;
       }
       setWinner(winner);
+      clearBoard();
     }
+  };
+
+  const setPlayers = (player1, player2) => {
+    players = [player1, player2];
+  };
+
+  const playAgain = () => {
+    clearBoard();
+    displayController.fillCells();
+    takeTurn();
   };
 
   const takeTurn = () => {
@@ -104,27 +212,8 @@ const gameboard = ((player1, player2) => {
     }
   };
 
-  return { getBoard, setCell, getCells, takeTurn };
-})(player1, player2);
-
-const game = (() => {})();
-
-const displayController = (() => {
-  const fillCells = (gameboard) => {
-    gameboard.getCells().forEach((cell) => {
-      const [index] = /\d+/.exec(cell.id);
-      cell.textContent = gameboard.getBoard()[index];
-    });
-  };
-  return { fillCells };
+  return { getBoard, setCell, getCells, takeTurn, setPlayers };
 })();
 
-gameboard.setCell(8, "X");
-gameboard.setCell(1, "0");
-gameboard.setCell(0, "0");
-gameboard.setCell(2, "X");
-gameboard.setCell(6, "0");
-console.log(gameboard.getBoard());
 displayController.fillCells(gameboard);
-gameboard.takeTurn();
 gameboard.takeTurn();
